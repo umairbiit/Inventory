ee6348bbdcdd54077749a6303f7e32b7c93b22d2
Talha Ayub <talhaayub789@gmail.com>
2024-11-15 01:39:35 +0500

    added user model

---
+++ server/models/userModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name !"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please add an email !"],
+    },
+    phoneno: {
+      type: String,
+      unique: true,
+      required: [true, "Please add a phone no. !"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required !"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Encrypt Password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  //Hashing
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

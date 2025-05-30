6491ebb14ea1c4d06cccd981c930347c83181b2e
Talha Ayub <talhaayub789@gmail.com>
2025-05-30 23:39:42 +0500

    check session validity API

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,28 @@ const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const loginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.json(false);
+  }
+  const verified = jwt.verify(token, process.env.JWT_SECRET);
+  if (verified) {
+    const user = await User.findById(verified.id).select("-password");
+    if (user) {
+      res.json({
+        verified: true,
+        id: verified.id,
+        status: user.status,
+        user: user,
+        token: token,
+      });
+    }
+  }
+
+  return res.json(false);
+});
+
 const logOut = asyncHandler(async (req, res) => {
   res.cookie("token", "", {
     path: "/",
@@ -15,5 +37,6 @@ const logOut = asyncHandler(async (req, res) => {
 });
 
 module.exports = {
+  loginStatus,
   logOut,
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const protect = require("../middlewares/authmiddleware");
-const { logOut } = require("../controllers/userController");
+const { logOut, loginStatus } = require("../controllers/userController");
 const router = express.Router();
 
+router.get("/loggedin-status", loginStatus);
 router.get("/logout", protect, logOut);

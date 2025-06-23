92fc0cec8f28af8ce6f08bbed70844daecd81156
Talha Ayub <talhaayub789@gmail.com>
2025-06-24 00:43:35 +0500

    react auth services added

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,9 +1,27 @@
 import axios from "axios";
-const API_URL = process.env.BACKEND_URL;
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const checkLoginStatus = async () => {
   try {
-    const response = await axios.get(`${API_URL}/loggedin-status`);
+    const response = await axios.get(`${API_URL}/api/auth/loggedin-status`);
+    return response;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const getUserDetails = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/api/auth/user-details`);
+    return response;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const logout = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/api/auth/logout`);
     return response;
   } catch (error) {
     return error.response.data;

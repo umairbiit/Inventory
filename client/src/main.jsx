8f9f202b95de1325252504eef8a5e44b07d0f4bc
Talha Ayub <talhaayub789@gmail.com>
2025-08-10 19:45:17 +0500

    antd theme added to main jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { UserProvider } from "./context/userContext.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { App as AntdApp, ConfigProvider } from "antd";
+import "antd/dist/reset.css";
+import "./index.css";
+import App from "./App";
+import { UserProvider } from "./context/userContext";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <UserProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserProvider>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#fdbc0a", // your orange color
+        },
+      }}
+    >
+      <AntdApp>
+        <UserProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </UserProvider>
+      </AntdApp>
+    </ConfigProvider>
+  </React.StrictMode>
 );
